4b4200eebeaff0d262ed7ddf0df7dfa89355ebb3
gruebite <gruebite@gmail.com>
2021-03-14 18:53:14 -0700

    Modify droplet.

--- entities/slime/droplet/droplet.gd
+++ entities/slime/droplet/droplet.gd
@@ -1,24 +1,15 @@
 extends Particles2D
 
-const SPEED := 64
+const SPEED := 128
 const NEAR := 4
 
-const BURST_TIME := 0.2
-const BURST_MULT := 3
+const WAIT_TIME := 0.3
 
-var burst := 0.0
-var bursting := true
-var burst_dir := Vector2.ZERO
-
-func _ready() -> void:
-    burst_dir = Vector2.ONE.rotated(Global.rng.randf() * TAU)
+var wait := 0.0
 
 func _process(delta: float) -> void:
-    if bursting:
-        position += burst_dir * delta * SPEED * BURST_MULT
-        burst += delta
-        if burst >= BURST_TIME:
-            bursting = false
+    if wait < WAIT_TIME:
+        wait += delta
     else:
         var pos_center = Global.space.player.position + Vector2(8, 0)
         var dir: Vector2 = (pos_center - position).normalized()
